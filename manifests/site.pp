0758cd435c46168e7710e12859e39ac7ea4452c5
Roman Savchuk <sav4ukrv@gmail.com>
2018-03-10 00:46:03 +0200

    remove version_minor

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,6 @@ node agent001 {
 node agent002 {
   include java8 {
     version_major => '161'
-    version_minor => 'b12'
     hash          => '2f38c3b165be4555a1fa6e98c45e0808'
   }
 }
--- modules/java8/manifests/init.pp
+++ modules/java8/manifests/init.pp
@@ -7,7 +7,6 @@
 # @example
 #  include java8 {
 #    version_major => '161'
-#    version_minor => 'b12'
 #    hash          => '2f38c3b165be4555a1fa6e98c45e0808'
 # }
 class java8 (
