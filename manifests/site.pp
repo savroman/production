819286ab96655b5bcf00414b88be7b777d604144
Roman Savchuk <sav4ukrv@gmail.com>
2018-03-10 00:48:43 +0200

    fix syntax mistakes

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node agent001 {
 }
 node agent002 {
   include java8 {
-    version_major => '161'
-    hash          => '2f38c3b165be4555a1fa6e98c45e0808'
+    version_major => '161',
+    hash          => '2f38c3b165be4555a1fa6e98c45e0808',
   }
 }
