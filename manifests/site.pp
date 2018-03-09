607921f574bc5f770b5a8cdda06c9cae0b94c04d
Roman Savchuk <sav4ukrv@gmail.com>
2018-03-10 00:33:52 +0200

    add version change

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,5 +3,9 @@ node agent001 {
   include java8
 }
 node agent002 {
-  include java8
+  include java8 {
+    version_major => '161'
+    version_minor => 'b12'
+    hash          => '2f38c3b165be4555a1fa6e98c45e0808'
+  }
 }
--- modules/java8/manifests/init.pp
+++ modules/java8/manifests/init.pp
@@ -1,24 +1,28 @@
 # java8
 #
-# A description of what this class does
+# This class instal JDK8
 #
 # @summary A short summary of the purpose of this class
 #
 # @example
-#   include java8
+#  include java8 {
+#    version_major => '161'
+#    version_minor => 'b12'
+#    hash          => '2f38c3b165be4555a1fa6e98c45e0808'
+# }
 class java8 (
-  $java_se = 'jdk',
-  $oracle_url = 'http://download.oracle.com/otn-pub/java/jdk/',
-  $cookie = '--no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"',
-  $version_major = '8u162',
+  $java_se       = 'jdk',
+  $oracle_url    = 'http://download.oracle.com/otn-pub/java/jdk/',
+  $cookie        = '--no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"',
+  $version_major = '162',
   $version_minor = 'b12',
-  $hash = '0da788060d494f5095bf8624735fa2f1',
-  $load_dir = "/tmp/",
+  $hash          = '0da788060d494f5095bf8624735fa2f1',
+  $load_dir      = "/tmp/",
   ) {
 
-  $rpm = "${java_se}-${version_major}-linux-x64.rpm"
-  $source = "${cookie} ${oracle_url}${version_major}-${version_minor}/${hash}/$rpm"
-  $java_path = "/usr/java/jdk1.8.0_162"
+  $rpm       = "${java_se}-8u${version_major}-linux-x64.rpm"
+  $source    = "${cookie} ${oracle_url}8u${version_major}-${version_minor}/${hash}/$rpm"
+  $java_path = "/usr/java/jdk1.8.0_${version_major}"
 
   # get JDK8 .rpm
   exec { 'upload_rpm':
@@ -51,3 +55,4 @@ class java8 (
   }
 }
 #http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm
+#http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.rpm
