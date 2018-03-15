af5594f9a683a7dab758f01346451857c30c4be2
oleksdiam <oleksdiam@yahoo.com>
2018-03-15 13:07:19 +0200

    fix site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,6 @@
 ## site.pp ##
 
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
-# used when an agent connects to a master and asks for an updated configuration.
-#
-# Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
 
-## Active Configurations ##
 
 # Disable filebucket by default for all File resources:
 File { backup => false }
@@ -31,9 +22,7 @@ node default {
 }
 # here is the line 32
 
-node 'test40.local' {
-  # This is where you can declare classes for all nodes.
-  # Example  
+node 'jenkins.local' {
   class { 'java8': 
     java_se       => 'jdk',
     # version_major => '162',
@@ -44,7 +33,7 @@ node 'test40.local' {
   include jenkins
 }
 
-node 'pclient.local' {
+node 'sonar.local' {
 
   class { 'java8':
     java_se       => 'jdk',
@@ -53,17 +42,4 @@ node 'pclient.local' {
   class { 'sonarqube':
   }
 }
-
-node 'jenkins.local' {
-
-  java::oracle { 'jdk8':
-    ensure        => 'present',
-    version_major => '8u162',
-    version_minor => 'b12',
-    url_hash      => '0da788060d494f5095bf8624735fa2f1',
-    java_se       => 'jdk',
-  }
-
-  include maven
-  include jenkins
-}                         
+                        
