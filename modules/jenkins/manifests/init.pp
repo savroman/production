3c7f3fa9eaa8beb797cb0c7b71ed229788de879a
Roman Savchuk <sav4ukrv@gmail.com>
2018-03-20 23:21:55 +0200

    fixed bug

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -79,7 +79,25 @@ class jenkins (
     enable     => true,
   }
 
-  firewall::openport { 'jenkins_port':
-    dport => ${dport},
+  #firewall::openport { 'jenkins_port':
+  #  dport => "${dport}",
+  #}
+  exec { 'firewall-cmd':
+    command => "firewall-cmd --zone=public --add-port=${dport}/tcp --permanent",
+    path    => "/usr/bin/",
+    before  => Exec['firewall-reload'],
+  }
+
+  exec { 'firewall-reload':
+    command => "firewall-cmd --reload",
+    path    => "/usr/bin/",
+    before  => Service['firewalld'],
+  }
+
+  service { 'firewalld':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    subscribe  => Exec['firewall-cmd'],
   }
 }
