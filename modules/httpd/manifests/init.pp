da681fe3256b2f897a9d69b292da21158e886eec
Roman Savchuk <sav4ukrv@gmail.com>
2018-03-25 18:44:29 +0300

    add rpmrepo module & some fixes in jenkins module

---
+++ modules/httpd/manifests/init.pp
@@ -0,0 +1,34 @@
+# httpd
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include httpd
+class httpd {
+  package {'httpd':
+    ensure => installed,
+    before => File['/etc/httpd/edit_httpdconf.sh'],
+  }
+
+  file { '/etc/httpd/edit_httpdconf.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => file( 'httpd/edit_httpdconf.sh'),
+    notify  => Exec['edit_httpd.conf'],
+  }
+
+  exec {'edit_httpd.conf':
+    command  => '/etc/httpd/edit_httpdconf.sh',
+    path     => '/bin:/sbin:/usr/bin:/usr/sbin'
+  }
+
+  service {'httpd':
+    ensure    => running,
+    enable    => true,
+    subscribe => Exec['edit_httpd.conf'],
+  }
+}
---
+++ modules/profile/manifests/rpmrepo.pp
@@ -0,0 +1,3 @@
+class profile::rpmrepo {
+  include rpmrepo
+}
