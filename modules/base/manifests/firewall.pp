7512dcb64f85df6213cb311e6224af93d9cc78ce
Roman Savchuk <sav4ukrv@gmail.com>
2018-03-25 23:39:29 +0300

    add define type firewall

--- modules/base/manifests/firewall.pp
+++ modules/base/manifests/firewall.pp
@@ -18,19 +18,19 @@ define base::firewall (
   $dport=''
   ) {
 
-  exec { 'firewall-cmd':
+  exec { 'firewall-cmd${title}':
     command => "firewall-cmd --zone=public --add-port=${dport}/tcp --permanent",
     path    => "/usr/bin/",
     before  => Exec['firewall-reload'],
   }
 
-  exec { 'firewall-reload':
+  exec { 'firewall-reload${title}':
     command => "firewall-cmd --reload",
     path    => "/usr/bin/",
     before  => Service['firewalld'],
   }
 
-  service { 'firewalld':
+  service { 'firewalld${title}':
     ensure     => running,
     enable     => true,
     hasrestart => true,
