0bc672eae06f3bb390c66a05c9a4176120ddba32
Bohdan Deputat <36072762+bdeputat@users.noreply.github.com>
2018-04-02 18:39:29 +0300

    module add user on base node

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,29 +1,25 @@
 # Class: base
-# ===========================
 #
-# Update os & install useful packeges
-#
-# @ param dport
-#    enter the number of port you wish to open
 #
 
 class base {
 
-  $packages = ['tree', 'mc', 'net-tools', 'wget', 'git']
+if $::osfamily == 'RedHat' {
+  exec { 'yum-update':
+      command      => '/usr/bin/yum -y update'
+      }
+}
 
-  if $::osfamily == 'RedHat' {
-    exec { 'yum-update':
-      command => '/usr/bin/yum -y update'
-    }
-  }
+elsif $::osfamily == 'Debian' {
 
-  elsif $::osfamily == 'Debian' {
     exec { 'apt-update':
-      command => '/usr/bin/apt-get -y update'
-    }
-  }
+      command      => '/usr/bin/apt-get -y update'
+      }
 
-  package { $packages:
+}
+
+$packages = ['tree', 'mc', 'net-tools']
+package { $packages:
     ensure       => installed,
-  }
+}
 }
