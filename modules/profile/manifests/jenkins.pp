12a22c60aec252db1ee50e49c68ba2c8b6788f80
Roman Savchuk <sav4ukrv@gmail.com>
2018-04-06 20:20:11 +0300

    add parameters to maven profile (#2)
    * add parameters to maven profile
    
    * make fixes according to review comments
    
    * fixed review comments except yumrepo

--- modules/profile/manifests/jenkins.pp
+++ modules/profile/manifests/jenkins.pp
@@ -1,4 +1,5 @@
 class profile::jenkins::master {
   include java8
   include jenkins
+  include maven3
 }
--- modules/role/manifests/jenkins.pp
+++ modules/role/manifests/jenkins.pp
@@ -1,6 +1,5 @@
 class role::jenkins::master {
     include profile::basenode
     include profile::jenkins::master
-    include profile::maven
     include profile::rpmrepo
 }
