36dcea10916c40b59f6d133171605c0e3caba3f9
Roman Savchuk <sav4ukrv@gmail.com>
2018-04-25 23:38:28 +0300

    add job manifest

---
+++ modules/jenkins/manifests/job.pp
@@ -0,0 +1,34 @@
+# == Define: jenkins::job
+#
+
+
+define jenkins::job (
+  $job_name,
+  $user,
+  $repoOwner,
+  $repository,
+  $interval,
+  ) {
+  $jobconf_hash = {
+    user       => $user,
+    repoOwner  => $repoOwner,
+    repository => $repository,
+    interval   => $interval,
+  }
+
+  file { "${jenkins::jenkins_home}/${job_name}":
+    ensure  => directory,
+    mode    => '0644',
+    owner   => 'tomcat',
+    group   => 'tomcat',
+  }
+
+  file { "${jenkins::jenkins_home}/${job_name}/config.xml":
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'tomcat',
+    group   => 'tomcat',
+    content => epp('jenkins/jobs/simplejob.xml.epp', $jobconf_hash),
+    require => File["${jenkins::jenkins_home}/${job_name}"],
+  }
+}
